63322a5f10c3d1c43d79b9e116f88d3d5bcead8c
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-10-22 13:32:44 +0530

    Day21 Project

---
+++ Angular8/Day21_project/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { UpdateemployeeComponent } from './updateemployee/updateemployee.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MyhttpService } from './myhttp.service';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HeaderComponent } from './header/header.component';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+import { MatConfirmDialogComponent } from './shared/mat-confirm-dialog/mat-confirm-dialog.component';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AddemployeeComponent,
+    UpdateemployeeComponent,
+    NotfoundComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule, //import animation
+    MaterialModule,
+    ToastrModule.forRoot({
+      timeOut:5000,
+      positionClass:'toast-top-right',
+      preventDuplicates:true
+    })
+  ],
+  providers: [AuthGuard,MyhttpService],
+  bootstrap: [AppComponent],
+  entryComponents:[MatConfirmDialogComponent]
+})
+export class AppModule { }
