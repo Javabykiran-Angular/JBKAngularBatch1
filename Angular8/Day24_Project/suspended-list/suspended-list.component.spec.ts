ae376698a692be50790a141a2a2ffa29661418c3
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-10-27 12:42:55 +0530

    Day24

---
+++ Angular8/Day24_Project/suspended-list/suspended-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuspendedListComponent } from './suspended-list.component';
+
+describe('SuspendedListComponent', () => {
+  let component: SuspendedListComponent;
+  let fixture: ComponentFixture<SuspendedListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SuspendedListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SuspendedListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Angular8/Day24_Project/suspended-list/suspended-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suspended-list',
+  templateUrl: './suspended-list.component.html',
+  styleUrls: ['./suspended-list.component.css']
+})
+export class SuspendedListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
